6d5d8ba94c014dcba4403b0652e4238b3c4061cc
Flavio W. Brasil <fwbrasil@gmail.com>
2012-04-27 01:09:19 -0300

    postgre

--- src/main/scala/com/example/foo/ActivateExampleContext.scala
+++ src/main/scala/com/example/foo/ActivateExampleContext.scala
@@ -6,6 +6,7 @@ import net.fwbrasil.activate.storage.memory.MemoryStorage
 //import net.fwbrasil.activate.storage.relational.SimpleJdbcRelationalStorage
 //import net.fwbrasil.activate.storage.relational.oracleDialect
 //import net.fwbrasil.activate.storage.relational.mySqlDialect
+//import net.fwbrasil.activate.storage.relational.postgresqlDialect
 //import net.fwbrasil.activate.storage.mongo.MongoStorage
 
 // Initially, must be created the context of Activate. The context must be a singleton, so it makes sense to declare as "object".
@@ -38,6 +39,14 @@ object activateExampleContext extends ActivateContext {
 	//	val password = "PASS"
 	//	val url = "jdbc:oracle:thin:@localhost:1521:oracle"
 	//	val dialect = oracleDialect
+	//}
+
+	//val storage = new SimpleJdbcRelationalStorage {
+	//	val jdbcDriver = "org.postgresql.Driver"
+	//	val user = "postgres"
+	//	val password = "teste"
+	//	val url = "jdbc:postgresql://127.0.0.1/postgres"
+	//	val dialect = postgresqlDialect
 	//}
 
 }
