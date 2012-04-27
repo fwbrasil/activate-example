0a8e36491533491307ecdc7358b3d97ce4ab7026
Flavio W. Brasil <fwbrasil@gmail.com>
2012-04-27 01:06:29 -0300

    0.8 release

--- project/ActivateExampleBuild.scala
+++ project/ActivateExampleBuild.scala
@@ -5,10 +5,10 @@ object ActivateExampleBuild extends Build {
 
 	/* Dependencies */
 	val fwbrasilRepo = "fwbrasil.net" at "http://fwbrasil.net/maven/"
-	val activateCore = "net.fwbrasil" %% "activate-core" % "0.6"
-	val activatePrevayler = "net.fwbrasil" %% "activate-prevayler" % "0.6"
-	val activateJdbc = "net.fwbrasil" %% "activate-jdbc" % "0.6"
-	val activateMongo = "net.fwbrasil" %% "activate-mongo" % "0.6"
+	val activateCore = "net.fwbrasil" %% "activate-core" % "0.8"
+	val activatePrevayler = "net.fwbrasil" %% "activate-prevayler" % "0.8"
+	val activateJdbc = "net.fwbrasil" %% "activate-jdbc" % "0.8"
+	val activateMongo = "net.fwbrasil" %% "activate-mongo" % "0.8"
 	val mysql = "mysql" % "mysql-connector-java" % "5.1.16"
 	/* 
     Install oracle in your local repo:
@@ -26,7 +26,7 @@ object ActivateExampleBuild extends Build {
 				libraryDependencies ++= Seq(activateCore /*, activatePrevayler, activateJdbc, activateMongo, mysql, objbd6*/ ),
 
 				organization := "com.example.foo",
-				scalaVersion := "2.9.1",
+				scalaVersion := "2.9.2",
 				version := "1.0",
 				resolvers += fwbrasilRepo))
 
